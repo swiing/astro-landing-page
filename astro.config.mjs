ff2250908c309d8efcbc4522629dd15ca366b3a9
swiing <persovince@yahoo.fr>
2025-03-23 08:45:32 +0100

    chore: add site url to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-landing-page-template.vercel.app/",
+
   vite: {
       plugins: [tailwindcss()]
 	},
